fa7184bb3b1d53cd42ed4b5a3570e4f185a2fa0c
spt4m1m <white.hat.tamim@gmail.com>
2023-05-26 12:08:19 +0600

    updated portfolio social links

--- src/Pages/Portfolio/PortfolioPage.js
+++ src/Pages/Portfolio/PortfolioPage.js
@@ -14,30 +14,30 @@ const Portfolio = () => {
                     <h1 className='text-3xl font-sans font-semibold text-center text-white py-2'>SP TAMIM</h1>
                     <p className='text-md font-sans font-semibold text-orange-500 text-center'>MERN Stack Developer</p>
                     <p className='text-sm font-sans font-semibold text-orange-500 text-center py-2'>sp.tam11m@gmail.com</p>
-                    <a href="https://sp-tamim.netlify.app/" className='block underline text-sm font-sans font-semibold text-blue-500 text-center py-2'>Personal Portfolio</a>
+                    <a href="https://sptamim.web.app/" className='block underline text-sm font-sans font-semibold text-blue-500 text-center py-2'>Personal Portfolio</a>
                     <div className='max-w-sm mx-auto'>
                         <ul class="flex flex-wrap items-center mt-5 text-sm sm:mb-0">
                             <li className='px-5 text-3xl'>
-                                <a href="https://www.facebook.com/Sp.Tamim33" target={blank} class="mr-4 md:mr-2 text-blue-500">
+                                <a href="https://www.facebook.com/spt4m1m" target={blank} class="mr-4 md:mr-2 text-blue-500">
                                     <Icon icon="il:facebook" /></a>
                             </li>
                             <li className='px-5 text-3xl'>
-                                <a href="https://www.instagram.com/sp.tamim33/" target={blank} class="mr-4 text-black md:mr-2">
+                                <a href="https://www.instagram.com/spt4m1m/" target={blank} class="mr-4 text-black md:mr-2">
                                     <div className='instagram'>
                                         <Icon icon="ant-design:instagram-filled" />
                                     </div>
                                 </a>
                             </li>
                             <li className='px-5 text-3xl'>
-                                <a href="https://twitter.com/SpTamim33" target={blank} class="mr-4 text-[#00b9ff] md:mr-6 ">
+                                <a href="https://twitter.com/spt4m1m" target={blank} class="mr-4 text-[#00b9ff] md:mr-6 ">
                                     <Icon icon="akar-icons:twitter-fill" /></a>
                             </li>
                             <li className='px-5 text-3xl'>
-                                <a href="https://github.com/Sp-Tamim33" target={blank} class="mr-4 text-orange-500 md:mr-6 ">
+                                <a href="https://github.com/spt4m1m" target={blank} class="mr-4 text-orange-500 md:mr-6 ">
                                     <Icon icon="akar-icons:github-fill" /></a>
                             </li>
                             <li className='px-5 text-3xl'>
-                                <a href="https://www.linkedin.com/in/sptamim33/" target={blank} class="mr-4 text-blue-500 md:mr-6 "><Icon icon="akar-icons:linkedin-box-fill" /></a>
+                                <a href="https://www.linkedin.com/in/spt4m11m/" target={blank} class="mr-4 text-blue-500 md:mr-6 "><Icon icon="akar-icons:linkedin-box-fill" /></a>
                             </li>
                         </ul>
                     </div>
